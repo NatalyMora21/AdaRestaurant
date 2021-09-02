7610b08c7640137c723d7d47ca5897d4a32b58f3
NatalyMora21 <tatamora21gmail.com>
2021-09-02 16:43:30 -0500

    created plate model

---
+++ src/db/restaurant/model/Plato.java
@@ -0,0 +1,53 @@
+package db.restaurant.model;
+
+public class Plato {
+	
+	private int id;
+	private String descripcion;
+	private double precio;
+	private int id_categoria;
+		
+	public Plato (String descripcion,double precio, int id_categoria ) {
+		
+		this.descripcion= descripcion;
+		this.precio= precio;
+		this.id_categoria =  id_categoria;
+		
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+	public int getId_categoria() {
+		return id_categoria;
+	}
+
+	public void setId_categoria(int id_categoria) {
+		this.id_categoria = id_categoria;
+	}
+	
+	
+
+}
