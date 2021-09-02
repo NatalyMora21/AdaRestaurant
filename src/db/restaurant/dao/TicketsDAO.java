f2afc5a71f979ee0d312b0543e6fd2ff6b7ea819
NatalyMora21 <tatamora21gmail.com>
2021-09-02 09:38:15 -0500

    Function created to add the total value in the ticket

--- src/db/restaurant/dao/TicketsDAO.java
+++ src/db/restaurant/dao/TicketsDAO.java
@@ -5,15 +5,25 @@ import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-
 import db.restaurant.model.Ticket;
 
 public class TicketsDAO {
+	
+	//Crar nuevo ticket
 	public static void insertarTicket(Ticket ticket, Connection connection) throws SQLException {
 		PreparedStatement stmt = connection.prepareStatement("INSERT INTO TICKET (NUMERO_MESA,  FECHA) VALUES(?,?)");
 		stmt.setInt(1, ticket.getMesa());
 		stmt.setDate(2, (Date) ticket.getFecha());
 		stmt.executeUpdate();
+	}
+	
+	//Modificar ticket, agregar valor total
+	public static void modificarTicket(Ticket ticket, Connection connection) throws SQLException {
+		PreparedStatement stmt = connection.prepareStatement("UPDATE TOTAL SET DESCRIPCION = ? WHERE ID = ?");
+		stmt.setDouble(1, ticket.getValorTotal());
+		stmt.setInt(2, ticket.getId());
+		stmt.executeUpdate();
+
 	}
 
 }
